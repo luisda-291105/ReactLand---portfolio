c81ad9b68f2814ab7f247e8592fe945c4b801a40
luisDaniel <estudiosluisperez@gmail.com>
2026-01-12 01:04:03 -0500

    refactor: remove unused router imports and commented-out code

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { HashRouter, BrowserRouter, Routes, Route } from 'react-router-dom'
-
 
 
 createRoot(document.getElementById('root')).render(
@@ -12,16 +10,3 @@ createRoot(document.getElementById('root')).render(
   </StrictMode>,
 )
 
-
-// Para GitHub Pages usa HashRouter
-function App() {
-  return (
-    <HashRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/contacto" element={<Contact />} />
-        {/* ... otras rutas */}
-      </Routes>
-    </HashRouter>
-  )
-}
